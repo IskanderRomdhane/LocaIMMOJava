fb1061374a0a795c59ac6716174643b2acb35601
Iskander Romdhane <skander20034@gmail.com>
2025-11-11 16:14:09 +0100

    Update Degradation.java

--- src/main/java/org/example/Entites/Degradation.java
+++ src/main/java/org/example/Entites/Degradation.java
@@ -3,23 +3,15 @@ package com.example.locaimmo.Entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Degradation {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String description;
     private double coutReparation;
     private boolean repare;
 
-    @ManyToOne
     private Logement logement;
 
-    @ManyToOne
     private Agent agentMenage;
 }
